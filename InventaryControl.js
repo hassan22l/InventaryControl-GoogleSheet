3ecce42b03e2c28b994a5f2bdd2b4c7899ac1ca4
MilagrosHassan <milagroshassan@gmail.com>
2023-11-22 10:19:38 -0300

    updateSearchPage

--- InventaryControl.js
+++ InventaryControl.js
@@ -13,8 +13,13 @@ function guardarProductos() {
   var equivalencias = hojaProductos.getRange("E5").getValue();
   var lugar = hojaProductos.getRange("G5").getValue();
   var cantidad = hojaProductos.getRange("I5").getValue();
+  var gananciafija = hojaProductos.getRange("H7").getValue();
+  var proveedor = hojaProductos.getRange("C7").getValue();
+  var tipoProducto = hojaProductos.getRange("E7").getValue();
+
+
   var hojaProductosInfo = libro.getSheetByName("Productos.Info")
-  hojaProductosInfo.appendRow([codigo, nombre, descripcion, precio, precioVenta, equivalencias, lugar])
+  hojaProductosInfo.appendRow([codigo, nombre, tipoProducto, descripcion, precio, precioVenta, equivalencias, lugar, gananciafija, proveedor])
 
   var hojaStock = libro.getSheetByName("Stock")
   var dataStock = hojaStock.getDataRange().getValues();
@@ -73,23 +78,20 @@ function guardarEntrada() {
       var aumento = hojaCompras.getRange("F7").getValue();
 
     if (isNaN(aumento) || aumento === "") {
-        Logger.log("No se proporcionó un número en la celda 'Aumentar'. Los precios permanecerán sin cambios.");
         return;
       }
 
     for(var i = 1; i <productos.length; i++) {
-        var marca = productos[i][8]
+        var marca = productos[i][9]
         var marcaACambiar = hojaCompras.getRange("D3").getValue()
 
           if (marcaACambiar === marca){
             
-              var precioActual = productos[i] [3];
+              var precioActual = productos[i] [4];
               var nuevoPrecio = precioActual * (1 + aumento / 100);
-              hojaProductos.getRange(i+ 1, 4).setValue(nuevoPrecio);
+              hojaProductos.getRange(i+ 1, 5).setValue(nuevoPrecio);
           }
       }
-
-
 
      var hojaStock = libro.getSheetByName("Stock")
       var dataStock = hojaStock.getDataRange().getValues();
@@ -174,4 +176,41 @@ function buscarProducto() {
   }
   hojaBusqueda.getRange('E2:E6').clearContent(); 
   hojaBusqueda.getRange('E2').setValue('Producto no encontrado');
+}
+
+
+function buscarProductoPorFiltro() {
+  var hojaProductos = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Productos.Info');
+  var hojaBusqueda = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Buscar');
+
+  var valorBuscado = hojaBusqueda.getRange('B4').getValue(); // Supongamos que el valor de búsqueda se encuentra en la celda A1 de la hoja "Buscar Productos".
+
+  // Obtén los datos de la hoja de productos.
+  var datosProductos = hojaProductos.getDataRange().getValues();
+
+  for (var i = 0; i < datosProductos.length; i++) {
+    var auto = datosProductos[i][3]; 
+
+    if (auto.toString().toLowerCase() === valorBuscado.toString().toLowerCase()) {
+      // Si se encuentra una coincidencia, muestra los datos en la hoja de búsqueda.
+      var codigo = datosProductos[i][0];
+      var nombre = datosProductos[i][1];
+      var precio = datosProductos[i][5]; // Supongamos que el precio está en la quita columna.
+      var equivalencia = datosProductos[i][6]; // Supongamos que la equivalencia está en la sexta columna.
+      var descripcion = datosProductos[i][3]; // Supongamos que la descripción está en la tercera columna.
+
+      // Llena los datos en la hoja de búsqueda.
+      hojaBusqueda.getRange('D2').setValue(codigo);
+      hojaBusqueda.getRange('D3').setValue(nombre);
+      hojaBusqueda.getRange('D7').setValue(precio);
+      hojaBusqueda.getRange('D5').setValue(equivalencia);
+      hojaBusqueda.getRange('D4').setValue(descripcion);
+
+      return; // Sal del bucle una vez que se haya encontrado una coincidencia.
+    }
+  }
+
+  // Si no se encontró una coincidencia, puedes mostrar un mensaje de error.
+  hojaBusqueda.getRange('D2:D6').clearContent(); // Borra los datos anteriores.
+  hojaBusqueda.getRange('B5').setValue('Producto no encontrado');
 }
